e3de4e34b30f2115d212c19164ae88cd92950f55
mannlex21 <maalmurilloma@ittepic.edu.mx>
2017-01-17 16:58:36 -0700

    Primera entrega, se creo el slidebar, sin la iconografia. Se dibujo linea, falta acomodo

---
+++ js/general/resize.js
@@ -0,0 +1,88 @@
+$(document).on('ready',function(){
+	cargado();
+});
+
+$(window).resize(function(){
+	$anchoInicial = window.innerWidth;
+	$escala = $anchoInicial / 1080;
+	$docePixeles = $escala * 12;
+	$trecePixeles = $escala * 13;
+	$catorcePixeles = $escala * 14;
+	$quincePixeles = $escala * 15;
+	$dieciseisPixeles = $escala * 16;
+	$diecisietePixeles = $escala * 17;
+	$dieciochoPixeles = $escala * 18;
+	$diesinuevePixeles = $escala * 19;
+	$veintePixeles = $escala * 20;
+	$veintiunPixeles = $escala * 21;
+	$veintidosPixeles = $escala * 22;
+	$veinticincoPixeles = $escala * 25;
+	$veintisietePixeles = $escala * 27;
+	$treintaYDosPixeles = $escala * 32;
+	$cuarentaPixeles = $escala * 40;
+	$cuarentaYCincoPixeles = $escala * 45;
+	$cincuentaYCincoPixeles = $escala * 55;
+	$cincuentaYSietePixeles = $escala * 57;
+
+	$('.docePixeles').css('font-size', $docePixeles + "px");
+	$('.trecePixeles').css('font-size', $trecePixeles + "px");
+	$('.catorcePixeles').css('font-size', $catorcePixeles + "px");
+	$('.quincePixeles').css('font-size', $quincePixeles + "px");
+	$('.dieciseisPixeles').css('font-size', $dieciseisPixeles + "px");
+	$('.diecisietePixeles').css('font-size', $diecisietePixeles + "px");
+	$('.dieciochoPixeles').css('font-size', $dieciochoPixeles + "px");
+	$('.diesinuevePixeles').css('font-size', $diesinuevePixeles + "px");
+	$('.veintePixeles').css('font-size', $veintePixeles + "px");
+	$('.veintiunPixeles').css('font-size', $veintiunPixeles + "px");
+	$('.veintidosPixeles').css('font-size', $veintidosPixeles + "px");
+	$('.veinticincoPixeles').css('font-size', $veinticincoPixeles + "px");
+	$('.veintisietePixeles').css('font-size', $veintisietePixeles + "px");
+	$('.treintaYDosPixeles').css('font-size', $treintaYDosPixeles + "px");
+	$('.cuarentaPixeles').css('font-size', $cuarentaPixeles + "px");
+	$('.cuarentaYCincoPixeles').css('font-size', $cuarentaYCincoPixeles + "px");
+	$('.cincuentaYCincoPixeles').css('font-size', $cincuentaYCincoPixeles + "px");
+	$('.cincuentaYSietePixeles').css('font-size', $cincuentaYSietePixeles + "px");
+});
+
+function cargado(){
+	$anchoInicial = window.innerWidth;
+	$escala = $anchoInicial / 1080;
+	$docePixeles = $escala * 12;
+	$trecePixeles = $escala * 13;
+	$catorcePixeles = $escala * 14;
+	$quincePixeles = $escala * 15;
+	$dieciseisPixeles = $escala * 16;
+	$diecisietePixeles = $escala * 17;
+	$dieciochoPixeles = $escala * 18;
+	$diesinuevePixeles = $escala * 19;
+	$veintePixeles = $escala * 20;
+	$veintiunPixeles = $escala * 21;
+	$veintidosPixeles = $escala * 22;
+	$veinticincoPixeles = $escala * 25;
+	$veintisietePixeles = $escala * 27;
+	$treintaYDosPixeles = $escala * 32;
+	$cuarentaPixeles = $escala * 40;
+	$cuarentaYCincoPixeles = $escala * 45;
+	$cincuentaYCincoPixeles = $escala * 55;
+	$cincuentaYSietePixeles = $escala * 57;
+
+	$('.docePixeles').css('font-size', $docePixeles + "px");
+	$('.trecePixeles').css('font-size', $trecePixeles + "px");
+	$('.catorcePixeles').css('font-size', $catorcePixeles + "px");
+	$('.quincePixeles').css('font-size', $quincePixeles + "px");
+	$('.dieciseisPixeles').css('font-size', $dieciseisPixeles + "px");
+	$('.diecisietePixeles').css('font-size', $diecisietePixeles + "px");
+	$('.dieciochoPixeles').css('font-size', $dieciochoPixeles + "px");
+	$('.diesinuevePixeles').css('font-size', $diesinuevePixeles + "px");
+	$('.veintePixeles').css('font-size', $veintePixeles + "px");
+	$('.veintiunPixeles').css('font-size', $veintiunPixeles + "px");
+	$('.veintidosPixeles').css('font-size', $veintidosPixeles + "px");
+	$('.veinticincoPixeles').css('font-size', $veinticincoPixeles + "px");
+	$('.veintisietePixeles').css('font-size', $veintisietePixeles + "px");
+	$('.treintaYDosPixeles').css('font-size', $treintaYDosPixeles + "px");
+	$('.cuarentaPixeles').css('font-size', $cuarentaPixeles + "px");
+	$('.cuarentaYCincoPixeles').css('font-size', $cuarentaYCincoPixeles + "px");
+	$('.cincuentaYCincoPixeles').css('font-size', $cincuentaYCincoPixeles + "px");
+	$('.cincuentaYSietePixeles').css('font-size', $cincuentaYSietePixeles + "px");
+}
+
